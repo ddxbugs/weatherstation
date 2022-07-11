e45c889f9740bc31e4eafdf35676a0626efad1e5
ddxbug <joseph.joo1@outlook.com>
2022-07-11 12:46:22 -0700

    initial commit weatherstation c and header file

---
+++ src/weatherstation.h
@@ -0,0 +1,19 @@
+// TODO: license
+
+#ifndef WEATHERSTATION_H
+#define WEATHERSTATION_H
+
+// define 
+// include 
+// TODO: include Arduino Core libraries
+// TODO: include NRFLite source header files in NRF24 folder
+enum type {};
+class weatherstation {
+    // default constructor
+    public:
+    // private constructor
+    private:
+    
+};
+
+#endif
