5ed40abf90f6b26760aa334b091f148a724defcd
ddxbug <joseph.joo1@outlook.com>
2022-07-17 16:07:13 -0700

    initial commit weather-client.ino

---
+++ example/weatherstation/weather-client.ino
@@ -0,0 +1,17 @@
+// include libraries
+
+#include <NRFLite.h>    // nrf radio library to communicate with weatherstation
+
+// global vars structs
+NRFLite radio;
+
+int pin_d1 = 1; // Digital sensor pin 1
+int pin_d2 = 2; // Digital sensor pin 2
+
+void setup() {
+    
+}
+
+void loop() {
+
+}
